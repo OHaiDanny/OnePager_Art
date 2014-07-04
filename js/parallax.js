b4d10c0e8de00671e887f9c8bedc6340182f2483
Danny Fang Yan <OHaiDanny>
2014-07-04 14:46:19 -0700

    added parallax effect

--- js/parallax.js
+++ js/parallax.js
@@ -1,17 +1,13 @@
 $(document).ready(function () {
-    $('section[data-type="background"]').each(function () {
+    $('div[data-type="background"]').each(function () {
         var $bgobj = $(this); // assigning the object
 
         $(window).scroll(function () {
-            var yPos = -($window.scrollTop() / $bgobj.data('speed'));
-
+            var yPos = -($(window).scrollTop() / $bgobj.data('speed'));
             // Put together our final background position
             var coords = '50% ' + yPos + 'px';
-
             // Move the background
-            $bgobj.css({
-                backgroundPosition: coords
-            });
+            $bgobj.css('background-position', coords);
         });
     });
 });
